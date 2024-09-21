cb02c816072c89667a6881fab29205ef16fbd489
Abid ali <khanali3229>
2024-09-21 10:02:07 +0500

    i selected the elements,which i give functionaly to it

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 'use strick';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelector('.show-modal');
+console.log(btnOpenModal);
